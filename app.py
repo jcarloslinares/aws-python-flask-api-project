e7bf1d44a7005d67af1498b6e9eaaafa436a2437
Juan Carlos Linares <jcarloslinares@gmail.com>
2022-12-07 11:09:26 -0500

    code commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_from_root():
+    return jsonify(message='OK!')
+
+
+@app.route("/hello")
+def hello():
+    return make_response(jsonify(message='Hello from path!'), 201)
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 404)
